2f053748f3a1cbaf0e42a4ef28e01946c295e9ab
minkong05 <186683024+minkong05@users.noreply.github.com>
2026-03-02 18:07:07 +0000

    feat: add core page skeletons (main, about, my account)

--- core/views.py
+++ core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, "core/home.html")
 
 def aboutus(request):
-    return HttpResponse("I haven't create the about.html yet")
+    return render(request, "core/aboutus.html")
+
+@login_required
+def main(request):
+    return render(request, "core/main.html")
