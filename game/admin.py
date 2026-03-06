42ab64eb113c1d4cd673b9a491bdaae3bd6791d1
minkong05 <186683024+minkong05@users.noreply.github.com>
2026-03-06 20:57:56 +0000

    feat: update admin.py with an example of UI-friendly version

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,21 @@
+# game/admin.py
 from django.contrib import admin
+from .models import EnemyType, Item, FriendType
+from .models import PlayerProfile, InventoryItem, PlayerFriend, Encounter
 
-# Register your models here.
+
+admin.site.register(Item)
+admin.site.register(FriendType)
+
+admin.site.register(PlayerProfile)
+admin.site.register(InventoryItem)
+admin.site.register(PlayerFriend)
+admin.site.register(Encounter)
+
+
+@admin.register(EnemyType)
+class EnemyTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "level", "max_hp", "damage", "reward_coins", "is_boss")
+    list_filter = ("level", "is_boss")
+    search_fields = ("name",)
+    ordering = ("level", "name")
